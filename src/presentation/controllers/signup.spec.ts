832d28639e4c5c6e14d979214cb83e0b80176bd8
JV-Amorim <joaovitoramorimx@gmail.com>
2021-07-27 19:55:18 -0300

    refactor: move EmailValidator creation to a factory helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -8,15 +8,27 @@ interface SutType {
   emailValidatorStub: EmailValidator;
 }
 
-const makeSut = (): SutType => {
+const makeEmailValidator = (): EmailValidator => {
   // Stub is a Mock type that returns always the same value.
   class EmailValidatorStub implements EmailValidator {
     isValid (email: string): boolean {
       return true;
     }
   }
+  return new EmailValidatorStub();
+};
 
-  const emailValidatorStub = new EmailValidatorStub();
+const makeEmailValidatorWithError = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      throw new Error();
+    }
+  }
+  return new EmailValidatorStub();
+};
+
+const makeSut = (): SutType => {
+  const emailValidatorStub = makeEmailValidator();
   const sut = new SignUpController(emailValidatorStub);
 
   return { sut, emailValidatorStub };
@@ -114,12 +126,7 @@ describe('SignUp Controller', () => {
   });
 
   test('Should return 500 if EmailValidator throws exception', () => {
-    class EmailValidatorStub implements EmailValidator {
-      isValid (email: string): boolean {
-        throw new Error();
-      }
-    }
-    const emailValidatorStub = new EmailValidatorStub();
+    const emailValidatorStub = makeEmailValidatorWithError();
     const sut = new SignUpController(emailValidatorStub);
     const httpRequest = {
       body: {
