1fc34aca15e2a3e2bc04e9bfa8e63ce678a14aee
JV-Amorim <joaovitoramorimx@gmail.com>
2021-07-28 10:34:26 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -119,7 +119,6 @@ describe('SignUp Controller', () => {
     const { sut, emailValidatorStub } = makeSut();
     // Below Jest is used to change the value always returned by the stub.
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false);
-
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -202,5 +201,25 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError());
+  });
+
+  test('Should return 200 if valid data is provided', () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@email.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'valid_password'
+    });
   });
 });
