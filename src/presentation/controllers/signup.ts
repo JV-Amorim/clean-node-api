10b60ae7a1e5ab0a9e7b5affe3b4ffc8601106ad
JV-Amorim <joaovitoramorimx@gmail.com>
2021-07-28 09:43:26 -0300

    refactor: remove code duplication

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -19,12 +19,14 @@ export class SignUpController implements Controller {
         }
       }
 
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      const { email, password, passwordConfirmation } = httpRequest.body;
+
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'));
       }
 
-      const isValid = this.emailValidator.isValid(httpRequest.body.email);
-      if (!isValid) {
+      const isEmailValid = this.emailValidator.isValid(email);
+      if (!isEmailValid) {
         return badRequest(new InvalidParamError('email'));
       }
     } catch (error) {
