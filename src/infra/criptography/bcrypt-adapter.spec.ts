ceb7fed522ffcab7d841658af4ad0cb1b1758917
JV-Amorim <joaovitoramorimx@gmail.com>
2021-07-30 20:42:55 -0300

    test: ensure BcryptAdapter return a hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt';
 import { BcryptAdapter } from './bcrypt-adapter';
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return Promise.resolve('valid_hash');
+  }
+}));
+
 describe('Bcrypt Adapter', () => {
   test('Should call Bcrypt with correct values', async () => {
     const rounds = 12;
@@ -8,5 +14,12 @@ describe('Bcrypt Adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.encrypt('any_value');
     expect(hashSpy).toHaveBeenCalledWith('any_value', rounds);
+  });
+
+  test('Should return a hash on success', async () => {
+    const rounds = 12;
+    const sut = new BcryptAdapter(rounds);
+    const hash = await sut.encrypt('any_value');
+    expect(hash).toBe('valid_hash');
   });
 });
